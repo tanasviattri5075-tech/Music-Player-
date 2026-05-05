8a61698a5ecb066a1ed076c11924f2f04a54f078
tanasviattri5075-tech <tanasvi.attri.5075@gmail.com>
2026-05-05 15:45:09 +0530

    Add files via upload

---
+++ music_list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "Waalian",
+        artist: "Harnoor",
+        img: "waalian",
+        src: "waalian"
+    },
+    {
+        name: "Choo Lo",
+        artist: "The Local Train",
+        img: "choo_lo",
+        src: "choo_lo"
+    },
+    {
+        name: "The Night We Met",
+        artist: "Lord Huron",
+        img: "night_we_met",
+        src: "night_we_met"
+    },
+    {
+        name: "Wishes",
+        artist: "Hasan Raheem",
+        img: "wishes",
+        src: "wishes"
+    },
+    {
+        name: "Phir Se Ud Chala",
+        artist: "Mohit Chauhan",
+        img: "phir_se_ud_chala",
+        src: "phir_se_ud_chala"
+    }
+];
---
+++ script.js
@@ -0,0 +1,62 @@
+const wrapper = document.querySelector(".wrapper"),
+musicImg = wrapper.querySelector("img"),
+musicName = wrapper.querySelector(".name"),
+musicArtist = wrapper.querySelector(".artist"),
+mainAudio = wrapper.querySelector("#main-audio"),
+playPauseBtn = wrapper.querySelector(".play-pause"),
+prevBtn = wrapper.querySelector("#prev"),
+nextBtn = wrapper.querySelector("#next"),
+progressSlider = document.querySelector("#progress-slider");
+
+let musicIndex = 1;
+
+window.addEventListener("load", () => {
+    loadMusic(musicIndex);
+});
+
+function loadMusic(index) {
+    musicName.innerText = allMusic[index-1].name;
+    musicArtist.innerText = allMusic[index-1].artist;
+
+    musicImg.src = "images/" + allMusic[index-1].img + ".jpg";
+    mainAudio.src = "songs/" + allMusic[index-1].src + ".mp3";
+}
+
+function playMusic() {
+    wrapper.classList.add("paused");
+    playPauseBtn.querySelector("button").innerText = "pause";
+    mainAudio.play();
+}
+
+function pauseMusic() {
+    wrapper.classList.remove("paused");
+    playPauseBtn.querySelector("button").innerText = "play_arrow";
+    mainAudio.pause();
+}
+
+playPauseBtn.addEventListener("click", () => {
+    wrapper.classList.contains("paused") ? pauseMusic() : playMusic();
+});
+
+nextBtn.addEventListener("click", () => {
+    musicIndex++;
+    if(musicIndex > allMusic.length) musicIndex = 1;
+    loadMusic(musicIndex);
+    playMusic();
+});
+
+prevBtn.addEventListener("click", () => {
+    musicIndex--;
+    if(musicIndex < 1) musicIndex = allMusic.length;
+    loadMusic(musicIndex);
+    playMusic();
+});
+
+mainAudio.addEventListener("timeupdate", () => {
+    progressSlider.max = mainAudio.duration;
+    progressSlider.value = mainAudio.currentTime;
+});
+
+progressSlider.addEventListener("input", () => {
+    mainAudio.currentTime = progressSlider.value;
+});
